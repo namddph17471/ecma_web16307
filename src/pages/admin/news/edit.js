43e924eabafe4a847502632b5ddbbd5d3c7f6875
namddph17471 <namddph17471@fpt.edu.vn>
2022-01-13 19:10:17 +0700

    lab3

--- src/pages/admin/news/edit.js
+++ src/pages/admin/news/edit.js
@@ -17,18 +17,40 @@ const EditNewPage = {
                         </div>
                     </div>
             </header>
-            <div class="max-w-5xl mx-auto">
-                <h1 class="font-bold uppercase mb-4 text-2xl">${found.title}</h1>
-                <img src="${found.img}" />
-                <p>${found.desc}</p>
-            </div>
-            <div class="mt-5 flex lg:mt-0 lg:ml-4">
-                <a href="/admin/news" class="sm:ml-3">
-                    <button type="button"
-                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Cập nhật
-                    </button>
-                </a>
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div class="px-4 py-6 sm:px-0">
+                    <form action="#" method="POST">
+                        <div class="shadow sm:rounded-md sm:overflow-hidden">
+                            <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                <div>
+                                    <label for="about" class="block text-sm font-medium text-gray-700">
+                                      Tiêu đề
+                                    </label>
+                                    <div class="mt-1">
+                                        <input type="text" value="${found.title}"  class="p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 py-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="">
+                                    </div>
+                                </div>
+                              <img src="${found.img}" />
+                                <div>
+                                    <label for="about" class="block text-sm font-medium text-gray-700">
+                                      Nội dung
+                                    </label>
+                                    <div class="mt-1">
+                                      <textarea id="about" name="about" rows="3" class=" p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" >${found.desc}</textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-5 flex lg:mt-0 lg:ml-4">
+                              <a href="/admin/news" class="sm:ml-3">
+                                  <button type="button"
+                                  class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                  Cập Nhật
+                                  </button>
+                              </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         `;
     },
