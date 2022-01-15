86f2730faaff16e856c8bfeec65c84f36c4f7c50
namddph17471 <namddph17471@fpt.edu.vn>
2022-01-15 12:29:34 +0700

    thêm vercel.json

--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -63,7 +63,7 @@ const AddNewsPage = {
                                 
                               </div>
                               <div class="mt-5 flex lg:mt-0 lg:ml-4">
-                                <a href="/admin/news" class="sm:ml-3">
+                                <a href="/admin/dasboard" class="sm:ml-3">
                                     <button type="button"
                                     class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                     Thêm mới tin tức
