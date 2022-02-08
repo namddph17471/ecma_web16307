3d7f0a9bf77f35cb954c9b2b8b1b7e2a7d00574a
namddph17471 <namddph17471@fpt.edu.vn>
2022-02-08 13:04:47 +0700

    thêm upload ảnh

--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -36,22 +36,8 @@ const AddNewsPage = {
                                 <label class="block text-sm font-medium text-gray-700">
                                   Ảnh
                                 </label>
-                                <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                    <div class="space-y-1 text-center">
-                                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                          <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                        <div class="flex text-sm text-gray-600">
-                                          <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                            <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                                          </label>
-                                          <p class="pl-1">or drag and drop</p>
-                                        </div>
-                                        <p class="text-xs text-gray-500">
-                                          PNG, JPG, GIF up to 10MB
-                                        </p>
-                                    </div>
+                                <div class="space-y-1 text-center">
+                                <input id="file-upload" type="file"  class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 py-1 block w-full sm:text-sm border border-gray-300 rounded-md" >
                                 </div>
                             </div>
                             <button
@@ -66,11 +52,25 @@ const AddNewsPage = {
     },
     afterRender() {
         const formAdd = document.querySelector("#form-add-post");
-        formAdd.addEventListener("submit", (e) => {
+        const imgPost = document.querySelector("#file-upload");
+        const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/namddph17471/image/upload";
+        const CLOUDINARY_PRESET = "nw9blvdh";
+        formAdd.addEventListener("submit", async (e) => {
             e.preventDefault();
+            const file = imgPost.files[0];
+
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("upload_preset", CLOUDINARY_PRESET);
+
+            const respone = await axios.post(CLOUDINARY_API, formData, {
+                headers: {
+                    "Content-Type": "application/form-data",
+                },
+            });
             const dataFake = {
                 title: document.querySelector("#title").value,
-                img: "http://placeimg.com/640/480/people",
+                img: respone.data.url,
                 desc: document.querySelector("#desc").value,
             };
             // call api thêm sản phẩm
